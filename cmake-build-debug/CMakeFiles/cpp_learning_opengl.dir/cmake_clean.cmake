d758682afe7c366f014a66496badcf9eb8d7b68e
Laurier Lavoie-Giasson <laurier.lavoie-giasson@umontreal.ca>
2019-01-24 23:53:53 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/cpp_learning_opengl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_learning_opengl.dir/src/main.cpp.o"
+  "cpp_learning_opengl.pdb"
+  "cpp_learning_opengl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_learning_opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ setup.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev xorg-dev
+
